306f841be6983449fa6c4a01b3f3c812615b9792
Renato Dinhani Conceição <renatodinhani@gmail.com>
2017-04-07 02:32:45 -0300

    Basic functionalities working

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,70 @@
 
 import * as vscode from 'vscode';
 const clipboardy = require('clipboardy');
+const debounce = require('debounce');
 
 // ============================================================================
 // EXTENSION INTERFACE
 // ============================================================================
 export function activate(context: vscode.ExtensionContext) {
-    vscode.window.onDidChangeTextEditorSelection(selection => {
-        let currentSelection = selection.selections[0];
-        let text = selection.textEditor.document.getText(currentSelection);
-        clipboardy.writeSync(text);
-    })
+    vscode.window.onDidChangeTextEditorSelection(debounce(event => {
+        if (shouldCopy(event)) {
+            let text = generateTextToCopy(event);
+            copyToClipboard(text);
+        }
+    }, 300))
 }
 
 export function deactivate() {
+}
+
+// =============================================================================
+// FUNCIONS
+// =============================================================================
+let config = vscode.workspace.getConfiguration("copyOnSelect");
+
+function shouldCopy(event: vscode.TextEditorSelectionChangeEvent): boolean {
+
+    // do not copy when text was selected by command
+    if (event.kind === vscode.TextEditorSelectionChangeKind.Command) {
+        return false;
+    }
+
+    // check keyboard copies
+    let copyOnKeyboard = config.get("copyOnKeyboardSelection", true);
+    if (event.kind === vscode.TextEditorSelectionChangeKind.Keyboard && !copyOnKeyboard) {
+        return false;
+    }
+
+    // check mouse copies
+    let copyOnMouse = config.get("copyOnMouseSelection", true);
+    if (event.kind === vscode.TextEditorSelectionChangeKind.Mouse && !copyOnMouse) {
+        return false;
+    }
+
+    // do not copy
+    return true;
+}
+
+function generateTextToCopy(event: vscode.TextEditorSelectionChangeEvent): string {
+    let eol = event.textEditor.document.eol == vscode.EndOfLine.LF ? '\n' : '\r\n';
+    var text = event.selections.map(selection => event.textEditor.document.getText(selection)).join(eol);
+
+    if (config.get("trimStart", false)) {
+        text = text.replace(/^\s+/, '');
+    }
+    if (config.get("trimEnd", false)) {
+        text = text.replace(/\s+$/, '');
+    }
+    return text;
+}
+
+function copyToClipboard(text: string) {
+    // do not copy empty text
+    if (text.trim() === '') {
+        return;
+    }
+
+    // copy
+    clipboardy.writeSync(text);
 }
